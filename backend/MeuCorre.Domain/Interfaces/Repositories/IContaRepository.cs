070107043b34fa427018391648f8362e4138a460
Pedro Henrique <senac.pedroholiveiramachado@gmail.com>
2025-09-30 21:28:54 -0300

    feat(backend): criação da interface do repositório de contas

---
+++ backend/MeuCorre.Domain/Interfaces/Repositories/IContaRepository.cs
@@ -0,0 +1,18 @@
+﻿using MeuCorre.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.Domain.Interfaces.Repositories
+{
+    public interface IContaRepository<Conta>
+    {
+        Task<List<Conta>> ObterPorUsuarioAsync(Guid usuarioId, bool apenasAtivas = true);
+        Task<List<Conta>> ObterPorTipoAsync(Guid usuarioId, TipoConta tipo);
+        Task<Conta?> ObterPorIdEUsuarioAsync(Guid contaId, Guid usuarioId);
+        Task<bool> ExisteContaComNomeAsync(Guid usuarioId, string nome, Guid? contaIdExcluir = null);
+        Task<decimal> CalcularSaldoTotalAsync(Guid usuarioId);
+    }
+}
